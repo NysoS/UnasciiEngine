e1da7094ea4a46aafa2bc29273273643b96ed5d2
Kristofer <k.ledoux.dev@gmail.com>
2026-01-16 21:50:48 +0100

    refacto architecture with separate engine and app

---
+++ Engine/include/Engine/Core/Application.hpp
@@ -0,0 +1,31 @@
+#ifndef APPLICATION_HPP
+#define APPLICATION_HPP
+
+#include "Engine/Engine.hpp"
+#include "Engine/Runtime/Platform/Window.hpp"
+
+#include <memory>
+
+class Window;
+
+namespace UnasciiEngine {
+	class UAE_API Application
+	{
+	public:
+		Application();
+		~Application();
+
+		void createWindow(const WindowInfo& pWinInfo);
+		void run();
+
+		inline Window& getWindow() { return *mWindow; }
+
+	private:
+		std::unique_ptr<Window> mWindow;
+		bool mIsRunning;
+
+		static constexpr float MaxFPS = 1.f / 60.f;
+	};
+}
+
+#endif // !APPLICATION_HPP
