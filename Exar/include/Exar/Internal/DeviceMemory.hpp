b6cafe7e96356c8710c0bc43267d0c5b4275e3f3
Kristofer <k.ledoux.dev@gmail.com>
2026-08-06 22:17:40 +0200

    create fence memory area

--- Exar/include/Exar/Internal/DeviceMemory.hpp
+++ Exar/include/Exar/Internal/DeviceMemory.hpp
@@ -25,13 +25,15 @@ namespace Exar
 		inline std::vector<const AreaMemory*> getAreaFamilies() noexcept {
 			return {
 				mSwapchainArea.get(),
-				mCommandPoolArea.get()
+				mCommandPoolArea.get(),
+				mFenceArea.get()
 			};
 		}
 
 	private:
 		std::unique_ptr<AreaMemory> mSwapchainArea;
 		std::unique_ptr<AreaMemory> mCommandPoolArea;
+		std::unique_ptr<AreaMemory> mFenceArea;
 	};
 }
 
